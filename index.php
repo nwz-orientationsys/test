5551058683e57f3b8c76aaebd5448f1ad3b30549
Ni <nwz@orientationsys.com>
2015-04-21 14:19:50 +0800

    what's the change on this commit? What issues are fixed on the commit?

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 /**
  *  Get CakePHP's root directory
  */
-define('APP_DIR', 'app');
+define('APP_DIR', 'app'  );
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__));
 define('WEBROOT_DIR', 'webroot');
@@ -38,4 +38,4 @@ if (!defined('CAKE_CORE_INCLUDE_PATH')) {
 	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
 }
 
-require APP_DIR . DS . WEBROOT_DIR . DS . 'index.php';
+
