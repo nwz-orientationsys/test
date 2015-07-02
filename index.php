6e994d534e2d8b1ce068a3ecf2ffacad887e819b
Nichol <nwz@orientationsys.com>
2015-07-02 14:32:56 +0800

    add b2

--- index.php
+++ index.php
@@ -29,6 +29,8 @@ define('ROOT', dirname(__FILE__));
 define('WEBROOT_DIR', 'webroot');
 define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
 
+echo 'add by b2';
+
 /**
  * This only needs to be changed if the "cake" directory is located
  * outside of the distributed structure.
