c336ff430ad9ed90f4963701d9c0bc78b897af6a
Nichol <nwz@orientationsys.com>
2015-07-02 14:40:00 +0800

    add b1

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
  * @since         CakePHP(tm) v 0.2.9
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
+echo 'add by b1';
 /**
  *  Get CakePHP's root directory
  */
